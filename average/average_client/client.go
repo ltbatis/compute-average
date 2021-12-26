5be7d7f23b7dfe5edc7acf38b2707a67a3e5c79b
Lucas Thiago Batista <lucas.batista@santander.com.br>
2021-12-26 15:24:49 -0300

    add client side implementation

--- average/average_client/client.go
+++ average/average_client/client.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
+	"time"
 
 	"github.com/ltbatista/compute-average/average/averagepb"
 	"google.golang.org/grpc"
@@ -25,5 +27,45 @@ func main() {
 
 func doClientStreaming(c averagepb.AverageServiceClient) {
 	fmt.Println("Sarting to do a Client Streaming RPC...")
-	//TODO: implementar client streaming requests
+	//TODO: implementar leitura de argumentos do terminal e percorrer por eles
+	requests := []*averagepb.AverageRequest{
+		&averagepb.AverageRequest{
+			Average: &averagepb.Average{
+				Number: 1,
+			},
+		},
+		&averagepb.AverageRequest{
+			Average: &averagepb.Average{
+				Number: 2,
+			},
+		},
+		&averagepb.AverageRequest{
+			Average: &averagepb.Average{
+				Number: 3,
+			},
+		},
+		&averagepb.AverageRequest{
+			Average: &averagepb.Average{
+				Number: 4,
+			},
+		},
+	}
+
+	stream, err := c.Average(context.Background())
+	if err != nil {
+		log.Fatalf("Error while calling Average: %v", err)
+	}
+
+	// we interate over our slice and send each message individually
+	for _, req := range requests {
+		fmt.Printf("Sending req: %v\n", req)
+		stream.Send(req)
+		time.Sleep(1000 * time.Millisecond)
+	}
+
+	res, err := stream.CloseAndRecv()
+	if err != nil {
+		log.Fatalf("error while receiving response from Averag: %v", err)
+	}
+	fmt.Printf("Average response: %v\n", res)
 }
